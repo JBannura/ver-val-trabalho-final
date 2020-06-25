cf941791b437db71c2e8c1f3d1c086144393e89a
gui-cavalheiro <guilherme.bonfada@pucrs.br>
2020-06-25 18:07:40 -0300

    Teste Passageiro

---
+++ src/test/java/grupo11/TestePassageiro.java
@@ -0,0 +1,32 @@
+package grupo11;
+
+import grupo11.interfaces.Persistencia.RepositorioPassageirosImplMem;
+import grupo11.entidades.Passageiro;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestePassageiro {
+
+    RepositorioPassageirosImplMem rp = new RepositorioPassageirosImplMem();
+    Passageiro p = rp.recuperaPorCPF("123456789");
+
+    @Test
+    public void testeGetPontuacaoMedia(){
+        Passageiro expected = rp.recuperaPorCPF("123456789");
+        Passageiro actual = p.novoPassageiro("123456789", "Ze");
+
+        Assertions.assertEquals(expected.getPontuacaoMedia(), actual.getPontuacaoMedia());
+    }
+    
+    @Test
+    public void testeInfoPontuacao(){
+        Passageiro expected = rp.recuperaPorCPF("123456789");
+        expected = expected.passageiroExistente("123456789", "Ze", 16, 2);
+
+        Passageiro actual = p.novoPassageiro("123456789", "Ze");
+        actual.infoPontuacao(8);
+        
+        Assertions.assertEquals(expected.getPontuacaoMedia(), actual.getPontuacaoMedia());
+    }
+}
